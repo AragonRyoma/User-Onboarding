aba7e0e96fb89f9a9de76f7a7dcdc2a2b7cd4c84
Leynier Aragon <aragonleynier@gmail.com>
2020-07-14 20:16:10 -0400

    starting out

--- onboarding/src/component/form.jsx
+++ onboarding/src/component/form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [form, setForm] = useState({ name: "" });
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
 
   const changeHandler = (e) => {
     console.log("input changed");
@@ -11,17 +11,33 @@ function Form() {
   const formSubmit = (e) => {
     e.preventDefault();
     console.log("form submitted");
+    console.log(form);
   };
 
   return (
     <form onSubmit={formSubmit}>
-      <label htmlFor="name">Name</label>
+      <label htmlFor="name">Name:</label>
       <input
+        id="name"
         type="text"
         name="name"
         value={form.name}
         onChange={changeHandler}
       ></input>
+
+      <label htmlFor="email">
+        Email:
+        <input id="email" type="email" name="email"></input>
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input id="password" type="password" name="password"></input>
+      </label>
+      <label htmlFor="terms" className="terms">
+        {" "}
+        Terms and Conditions
+        <input type="checkbox" name="terms" checked></input>
+      </label>
       <button>click me!</button>
     </form>
   );
